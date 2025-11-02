b5c1685a982bc452914d592e227a66587a39f2f8
idalopban <168684966+idalopban@users.noreply.github.com>
2025-11-01 20:18:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import sys # Importado para compatibilidad de rutas de archivos
 # --- Configuración de la Página ---
 st.set_page_config(
     page_title="ComVida",
-    page_icon="🍋",
+    page_icon="🍋‍🟩",
     layout="wide",
     initial_sidebar_state="expanded"
 )
@@ -1817,3 +1817,4 @@ def main():
 if __name__ == "__main__":
 
     main()
+
